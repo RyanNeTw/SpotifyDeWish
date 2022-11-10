8f3bd5d57991baf5477f1fb4ca0c72e8c96be153
Neil Richter <me@neilrichter.com>
2022-11-10 10:56:15 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { instance } from '@/api';
+import { search } from '@/api/spotify';
+import { ref } from 'vue';
+
+const artists = ref([]);
+
+async function init() {
+  artists.value = await search('Flume', 'artist')
+}
+
+init()
+</script>
+
+<template>
+  <main class="p-16 bg-[#E1E9ED]">
+   <pre>{{ artists }}</pre>
+  </main>
+</template>
